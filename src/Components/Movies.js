3ec5c3611fe8efac25e484390cce1ffe23f8b9a1
kristin-7 <dingchuanhu@ucsb.edu>
2020-05-20 16:14:45 +0800

    Integrate AllMovies into Movies Page

--- src/Components/Movies.js
+++ src/Components/Movies.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react"
+import SimpleReactLightbox, { SRLWrapper } from "simple-react-lightbox"
 import Dropdown from "react-bootstrap/Dropdown"
 import DropdownButton from "react-bootstrap/DropdownButton"
-import AllMovies from "./AllMovies"
 import AddMovie from "./AddMovie"
 import CreateList from "./CreateList"
 import config from "../config"
 import "./Movies.css"
 
+const axios = require('axios');
 const firebase = require('firebase');
 
 export class Movies extends Component {
@@ -14,8 +15,56 @@ export class Movies extends Component {
     super();
     this.state = {
       lists: [],
+      movies: [],
     }
     this.loadList();
+    this.loadMovies();
+  }
+
+  loadMovies() {
+    if (!firebase.apps.length) {
+      firebase.initializeApp(config)
+    }
+    let ref = firebase.database().ref('movieIDs');
+    ref.on('value', snapshot => {
+      let data = snapshot.val();
+      console.log(data);
+      for (let entry in data) {
+        console.log(data[entry].id);
+        this.getRequest(data[entry].id);
+      }
+    })
+  }
+
+  getRequest(id) {
+    let req = 'https://www.omdbapi.com/?apikey=909a9c3d&i='+id;
+    console.log("Requesting");
+    axios.get(req)
+    .then(response => {
+      console.log("Get");
+      const movie = {
+        poster: response.data.Poster,
+        title: response.data.Title,
+        director: response.data.Director,
+        rating: response.data.imdbRating,
+      }
+      
+      this.setState({movies: [...this.state.movies, movie]})
+    })
+    .catch(function(error){
+      console.log("Error")
+    })
+    .then(function(){
+      return;
+    });
+  }
+
+  dimPoster = (e) => {
+    e.target.style.filter= 'brightness(40%)';
+  }
+
+  undimPoster = (e) => {
+    e.target.style.filter= 'brightness(100%)';
   }
 
   loadList() {
@@ -26,6 +75,16 @@ export class Movies extends Component {
   }
 
   render() {
+    const Movies = this.state.movies && this.state.movies.map(({poster, title, director, rating}) => {
+      return (
+        <img src={poster}
+        onMouseEnter={this.dimPoster}
+        onMouseLeave={this.undimPoster}
+        alt={title + "\n" + "Director: " + director + "\n" + "IMDB Rating: " + rating + "🌟"}>
+        </img>
+      );
+    })
+
     return (
       <div className='movies'>
         <div className='movie-nav-bar'>
@@ -39,7 +98,13 @@ export class Movies extends Component {
           <CreateList />
         </div>
         <div className='movie-details'>
-          <AllMovies />
+          <SimpleReactLightbox>
+            <SRLWrapper>
+              <div className="Movie-grid">
+                {Movies}
+              </div>
+            </SRLWrapper>
+          </SimpleReactLightbox>
         </div>
       </div>
     );
